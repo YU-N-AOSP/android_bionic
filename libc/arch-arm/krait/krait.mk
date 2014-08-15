fb842a0b72f9b00b43ea9628965364dc7a7814e6
Xin Qi <xqi@codeaurora.org>
2014-08-15 11:12:32 -0700

    Performance: krait: Implement optimized versions of memmove
    Code has been refactored to thumb2 for consistency with the rest
    of bionic libc, as well as performance and correctness.
    
    Change-Id: I5f738ef3eb12ece6b55285f1588eab3d4bbbe27d

--- libc/arch-arm/krait/krait.mk
+++ libc/arch-arm/krait/krait.mk
@@ -19,6 +19,7 @@ libc_bionic_src_files_arm += \
     arch-arm/krait/bionic/strcmp.S \
     arch-arm/krait/bionic/__strcat_chk.S \
     arch-arm/krait/bionic/__strcpy_chk.S \
+    arch-arm/krait/bionic/memmove.S
 
 # Use cortex-a15 versions of strcat/strcpy/strlen and standard memmove
 libc_bionic_src_files_arm += \
@@ -26,6 +27,3 @@ libc_bionic_src_files_arm += \
     arch-arm/cortex-a15/bionic/strcat.S \
     arch-arm/cortex-a15/bionic/strcpy.S \
     arch-arm/cortex-a15/bionic/strlen.S \
-
-libc_bionic_src_files_arm += \
-    arch-arm/denver/bionic/memmove.S \
